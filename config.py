6b5f2bb263148b78ca74787582c5cfe76ffa11b2
Lerro <makar1212tg@gmail.com>
2025-12-27 21:39:30 +0400

    Add configuration class with API credentials

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config:
+    API_ID = 21482458  # Bura rəqəm yaz
+    API_HASH = "577573e5e3e8db6af7e680dcdde0f0da" 
+    BOT_TOKEN = "8525511856:AAHVCpB4pKSj9n4WR_OCFtYVh6gDWVQ2LzU"
+  
